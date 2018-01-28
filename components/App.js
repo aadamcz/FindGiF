791b94ce86e8e6cb6f4f02c2d4a324bf5d3fa444
Agnieszka Adamczyk <adamcz.aga@gmail.com>
2018-01-28 09:04:54 +0100

    Update App.js

--- components/App.js
+++ components/App.js
@@ -1,5 +1,5 @@
-var GIPHY_API_URL = 'https://api.giphy.com',
-GIPHY_PUB_KEY = 'rba1HbESsSPo547i5ubO7ouD8X3onJ72';
+var GIPHY_API_URL = "https://api.giphy.com",
+    GIPHY_PUB_KEY = "rba1HbESsSPo547i5ubO7ouD8X3onJ72";
 
 App = React.createClass({
     getInitialState() {
@@ -10,38 +10,9 @@ App = React.createClass({
         };
     },
 
-   /*getGif: function(searchingText, callback) {
-        var url =
-            GIPHY_API_URL +
-            "/v1/gifs/random?api_key=" +
-            GIPHY_PUB_KEY +
-            "&tag=" +
-            searchingText;
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", url);
-        xhr.onload = function() {
-            if (xhr.status === 200) {
-                var data = JSON.parse(xhr.responseText).data;
-                var gif = {
-                    url: data.fixed_width_downsampled_url,
-                    sourceUrl: data.url
-                };
-                callback(gif);
-            }
-        };
-        xhr.send();
-    },
-    */
-    //getGif with Promise
-
-    getGif: function(searchingText){
+    getGif: function(searchingText) {
         return new Promise((resolve, reject) => {
-            var url =
-            GIPHY_API_URL +
-            "/v1/gifs/random?api_key=" +
-            GIPHY_PUB_KEY +
-            "&tag=" +
-            searchingText;
+            var url = GIPHY_API_URL + "/v1/gifs/random?api_key=" + GIPHY_PUB_KEY + "&tag=" + searchingText;
             var xhr = new XMLHttpRequest();
             xhr.open("GET", url);
             xhr.onload = function() {
@@ -52,18 +23,17 @@ App = React.createClass({
                 		sourceUrl: data.url
                 	};
                 resolve(gif);
-            }
-        };
+                }
+            };
         xhr.send();
+        })
     },
-
+ 
     handleSearch: function(searchingText) {
         this.setState({
             loading: true
         });
-        this.getGif(
-            searchingText,
-            function(gif) {
+        this.getGif(searchingText).then((gif)=> {
                 this.setState({
                     loading: false,
                     gif: gif,
@@ -84,9 +54,7 @@ App = React.createClass({
             <div style={styles}>
                 <h1>Wyszukiwarka GIFow!</h1>
                 <p>
-                    Znajdź gifa na <a href="http://giphy.com">giphy</a>.
-                    Naciskaj enter, aby pobrać kolejne gify.
-                </p>
+                    Znajdź gifa na <a href="http://giphy.com">giphy</a> Naciskaj enter, aby pobrać kolejne gify.</p>
                 <Search onSearch={this.handleSearch} />
                 <Gif
                     loading={this.state.loading}
